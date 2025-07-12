65fef7f8063ac26ac52bff3e665c2e20744036ae
ShukakuZa <yusufzuzu787@gmail.com>
2025-07-12 20:11:30 +0000

    surya: added `vendorsetup.sh` for mist

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://codeberg.org/Zabuka/vendor_xiaomi_surya.git -b 16 vendor/xiaomi/surya
+git clone https://github.com/Evolution-X-Devices/hardware_xiaomi.git -b bka hardware/xiaomi
+git clone https://github.com/yusufnoor786/device_xiaomi_surya.git -b test kernel/xiaomi/surya
